f1e65c592c40c3e7234d1a01bb0a6fdd07cf7fe3
Pratham Vaidya <iamprathamvaidya@gmail.com>
2024-03-23 21:21:05 +0530

    frontend

--- app/assets/javascripts/components/onboarding/form.jsx
+++ app/assets/javascripts/components/onboarding/form.jsx
@@ -107,8 +107,6 @@ const Form = (props) => {
           <p className="form-help-text">
             Your real name is not public. It is only seen by you, your
             instructor, and Wiki Education admins.
-
-
           </p>
         </div>
         <div className="form-group">
